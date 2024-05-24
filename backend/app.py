bf68c91ce2f9cdad0e3d4709413cbd3b21927fb0
jating378 <100723944+jating378@users.noreply.github.com>
2024-05-24 09:47:59 +0200

    Separate file for connection
    Made db.py which can be used to for connection to mysql and can be imported directly

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,10 @@
 from flask import Flask, jsonify, request
 import mysql.connector
 from mysql.connector import Error
+from db import create_connection
 
 app = Flask(__name__)
 
-def create_connection():
-    try:
-        connection = mysql.connector.connect(
-            host='c-kohn.lu',
-            user='c1SAD',
-            password='SAD-SRH2024',
-            database='c1sad'
-        )
-        return connection
-    except Error as e:
-        print(f"Error: {e}")
-        return None
 
 @app.route('/data', methods=['GET'])
 def get_data():
@@ -25,7 +14,7 @@ def get_data():
 
     cursor = connection.cursor(dictionary=True)
     try:
-        cursor.execute("SELECT * FROM users")  
+        cursor.execute("SELECT * FROM users")
         rows = cursor.fetchall()
         return jsonify(rows)
     except Error as e:
---
+++ backend/db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='c-kohn.lu',
+            user='c1SAD',
+            password='SAD-SRH2024',
+            database='c1sad'
+        )
+        return connection
+    except Error as e:
+        print(f"Error: {e}")
+        return None
