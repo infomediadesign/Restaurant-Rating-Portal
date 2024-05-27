d709d43ccc11850a65bf4cb0447d333ba02f25f6
Riddhi816 <71248561+Riddhi816@users.noreply.github.com>
2024-05-27 20:26:21 +0200

    created a RestaurantCard which will dynamically generate list of restaurants

---
+++ frontend/rating-portal/src/components/Restaurant/RestaurantCard.js
@@ -0,0 +1,39 @@
+    import React from 'react';
+
+    const RestaurantCard = ({ restaurant }) => {
+    return (
+        <div
+        style={{
+            width: '300px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            padding: '10px',
+            margin: '10px',
+            cursor: 'pointer',
+        }}
+        >
+        <img
+            src={restaurant.image}
+            alt={restaurant.name}
+            style={{
+            width: '100%',
+            borderRadius: '5px',
+            marginBottom: '10px',
+            }}
+        />
+        <h3>{restaurant.name}</h3>
+        <p>{restaurant.location}</p>
+        <p>Rating: {restaurant.rating}</p>
+        {/* Stars for the rating */}
+        <div>
+            {Array.from({ length: 5 }, (_, index) => (
+            <span key={index} style={{ color: index < restaurant.rating ? 'gold' : '#ccc' }}>
+                ★
+            </span>
+            ))}
+        </div>
+        </div>
+    );
+    };
+
+    export default RestaurantCard;
