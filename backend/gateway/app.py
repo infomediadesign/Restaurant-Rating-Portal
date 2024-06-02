4838aac0e2f793ce2a9d00ff72215f6b314839c0
jating378 <100723944+jating378@users.noreply.github.com>
2024-06-02 15:54:59 +0200

    Added userservice api calls to gatway
    Added all sub api calls of userservice to gateway

--- backend/gateway/app.py
+++ backend/gateway/app.py
@@ -2,22 +2,61 @@ from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 import bcrypt
 import os
+import requests
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.json.sort_keys = False
 auth = HTTPBasicAuth()
 
-
 @auth.verify_password
 def verify_password(username, password):
-    api_key_encrypted = os.getenv("GATEWAY_USERNAME")
+    gateway_username = os.getenv("GATEWAY_USERNAME")
+    gateway_password_encrypted = os.getenv("GATEWAY_PASSWORD_ENCRYPTED")
 
-    if username != os.getenv("GATEWAY_PASSWORD"):
+    if username != gateway_username:
         return False
-    if not bcrypt.checkpw(password.encode('utf-8'), api_key_encrypted.encode('utf-8')):
+    if not bcrypt.checkpw(password.encode('utf-8'), gateway_password_encrypted.encode('utf-8')):
         return False
     return True
+
+USER_SERVICE_URL = "http://localhost:5001"
+
+@app.route('/users/register', methods=['POST'])
+@auth.login_required
+def register_route():
+    response = requests.post(f"{USER_SERVICE_URL}/register", json=request.json, auth=(os.getenv("USERS_API_USERNAME"), os.getenv("USERS_API_PASSWORD")))
+    return jsonify(response.json()), response.status_code
+
+@app.route('/users/login', methods=['POST'])
+@auth.login_required
+def login_route():
+    response = requests.post(f"{USER_SERVICE_URL}/login", json=request.json, auth=(os.getenv("USERS_API_USERNAME"), os.getenv("USERS_API_PASSWORD")))
+    return jsonify(response.json()), response.status_code
+
+@app.route('/users/fetch_by_id', methods=['POST'])
+@auth.login_required
+def get_user_data_route():
+    response = requests.post(f"{USER_SERVICE_URL}/fetch_by_id", json=request.json, auth=(os.getenv("USERS_API_USERNAME"), os.getenv("USERS_API_PASSWORD")))
+    return jsonify(response.json()), response.status_code
+
+@app.route('/users/fetch_all', methods=['GET'])
+@auth.login_required
+def fetch_all_users_route():
+    response = requests.get(f"{USER_SERVICE_URL}/fetch_all", auth=(os.getenv("USERS_API_USERNAME"), os.getenv("USERS_API_PASSWORD")))
+    return jsonify(response.json()), response.status_code
+
+@app.route('/users/update', methods=['POST'])
+@auth.login_required
+def update_user_data_route():
+    response = requests.post(f"{USER_SERVICE_URL}/update", json=request.json, auth=(os.getenv("USERS_API_USERNAME"), os.getenv("USERS_API_PASSWORD")))
+    return jsonify(response.json()), response.status_code
+
+@app.route('/users/delete', methods=['POST'])
+@auth.login_required
+def delete_user_route():
+    response = requests.post(f"{USER_SERVICE_URL}/delete", json=request.json, auth=(os.getenv("USERS_API_USERNAME"), os.getenv("USERS_API_PASSWORD")))
+    return jsonify(response.json()), response.status_code
 
 
 if __name__ == '__main__':
--- backend/user_service/app.py
+++ backend/user_service/app.py
@@ -72,5 +72,5 @@ def delete_user_route():
 
 if __name__ == '__main__':
     load_dotenv()
-    app.run(debug=True)
+    app.run(debug=True , port = 5001)
 
