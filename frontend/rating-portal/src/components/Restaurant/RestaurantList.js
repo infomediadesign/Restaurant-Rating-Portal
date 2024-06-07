31310b82504f057d547591bac6f413aa38c4ce50
Riddhi816 <71248561+Riddhi816@users.noreply.github.com>
2024-06-07 18:15:32 +0200

    logic implementation of fetching restaurant list

--- frontend/rating-portal/src/components/Restaurant/RestaurantList.js
+++ frontend/rating-portal/src/components/Restaurant/RestaurantList.js
@@ -1,30 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { fetchRestaurants } from '../../api';
+import axios from 'axios';
 
 const RestaurantList = () => {
     const [restaurants, setRestaurants] = useState([]);
 
     useEffect(() => {
-        const getRestaurants = async () => {
+        const fetchRestaurants = async () => {
             try {
-                const data = await fetchRestaurants();
-                setRestaurants(data);
+                const response = await axios.post('http://127.0.0.1:5000/restaurants/fetch_all', {}, {
+                    headers: {
+                        'Authorization': `Basic ${btoa('api_gateway:Xe812C81M9yA')}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setRestaurants(response.data);
             } catch (error) {
                 console.error('Error fetching restaurants:', error);
             }
         };
-        getRestaurants();
+
+        fetchRestaurants();
     }, []);
 
     return (
         <div>
-            <h1>Restaurants</h1>
+            <h1>Restaurants Near You</h1>
             <ul>
-                {restaurants.map((restaurant) => (
-                    <li key={restaurant.id}>
+                {restaurants.map(restaurant => (
+                    <li key={restaurant.pk_restaurant}>
                         <h2>{restaurant.name}</h2>
-                        <p>{restaurant.description}</p>
-                        <p>Rating: {restaurant.rating}</p>
+                        <p>Genre: {restaurant.genre}</p>
+                        <p>Address: {`${restaurant.house_number} ${restaurant.street_name}, ${restaurant.city}, ${restaurant.state}, ${restaurant.zip_code}, ${restaurant.country}`}</p>
+                        <p>License: {restaurant.licence}</p>
+                        {restaurant.verified ? <p>Verified</p> : <p>Not Verified</p>}
+                        {restaurant.images && restaurant.images.map(image => (
+                            <img key={image} src={image} alt={`${restaurant.name}`} />
+                        ))}
                     </li>
                 ))}
             </ul>
