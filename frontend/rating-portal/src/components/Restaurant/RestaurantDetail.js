76d22e2334a08c5c74103ed41f1fcba4b8c1c237
Riddhi816 <71248561+Riddhi816@users.noreply.github.com>
2024-06-09 00:34:56 +0200

    modified RestaurantDetail.js

--- frontend/rating-portal/src/components/Restaurant/RestaurantDetail.js
+++ frontend/rating-portal/src/components/Restaurant/RestaurantDetail.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { UserContext } from '../UserContext/UserContext.js';
 
 const RestaurantDetail = () => {
     const { id } = useParams();
     const [restaurant, setRestaurant] = useState(null);
     const [rating, setRating] = useState('');
+    const [review, setReview] = useState("");
     const [reply, setReply] = useState('');
+    const { user } = useContext(UserContext);
 
     useEffect(() => {
         const fetchRestaurantDetails = async () => {
@@ -24,15 +27,24 @@ const RestaurantDetail = () => {
             }
         };
         fetchRestaurantDetails();
-    }, []);
+    }, [id]);
 
     const handleRatingSubmit = async () => {
         try {
-            await axios.post('http://127.0.0.1:5000/ratings/create', {
-                fk_restaurant: id,
-                rating
-            });
+            await axios.post("http://127.0.0.1:5000/ratings/create", { 
+                    fk_user: user.pk_user,
+                    fk_restaurant: id,
+                    stars:rating,
+                    review: review
+                }, {
+                    headers: {
+                        'Authorization': `Basic ${btoa('api_gateway:Xe812C81M9yA')}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
             alert('Rating submitted successfully!');
+            setRating('');
+            setReview('');
         } catch (error) {
             console.error('Error submitting rating:', error);
         }
@@ -45,6 +57,7 @@ const RestaurantDetail = () => {
                 reply
             });
             alert('Reply submitted successfully!');
+            setReply("");
         } catch (error) {
             console.error('Error submitting reply:', error);
         }
@@ -55,7 +68,8 @@ const RestaurantDetail = () => {
         <div>
             <h1>{restaurant?.name}</h1>
             <p>{restaurant?.description}</p>
-            <input type="text" value={rating} onChange={(e) => setRating(e.target.value)} placeholder="Enter rating" />
+            <input type="text" value={rating} onChange={(e) => setRating(e.target.value)} placeholder="Enter stars" />
+            <input type="text" value={review} onChange={(e) => setReview(e.target.value)} placeholder="Enter review" />
             <button onClick={handleRatingSubmit}>Submit Rating</button>
             <input type="text" value={reply} onChange={(e) => setReply(e.target.value)} placeholder="Enter reply" />
             <button onClick={handleReplySubmit}>Submit Reply</button>
