2206b53b97dac78802cfb022be5db48504d77a33
RohitSardeshmukh <sardeshmukhrohit@gmail.com>
2024-06-09 11:46:49 +0200

    Navbar working now

--- frontend/rating-portal/src/components/Navbar.js
+++ frontend/rating-portal/src/components/Navbar.js
@@ -2,25 +2,26 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 import './Navbar.css';
-import ProfileDropdown from './ProfileDropdown/ProfileDropdown';
-import { UserContext } from './UserContext/UserContext'
+import MyReviewsDropdown from '../components/ProfileDropdown/ProfileDropdown';
+import { UserContext } from '../components/UserContext/UserContext';
 
 const Navbar = () => {
     const { user } = useContext(UserContext);
 
     return (
         <nav className="nav">
-            <img src={logo} alt="Logo" className="logo" />
+            <div>
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
             <div>
                 <Link to="/" className="nav-link">Home</Link>
-                {!user ? (
+                {!user && (
                     <>
                         <Link to="/register-user" className="nav-link">Register</Link>
                         <Link to="/login" className="nav-link">Login</Link>
                     </>
-                ) : (
-                    <ProfileDropdown />
                 )}
+                {user && <MyReviewsDropdown />}
             </div>
         </nav>
     );
