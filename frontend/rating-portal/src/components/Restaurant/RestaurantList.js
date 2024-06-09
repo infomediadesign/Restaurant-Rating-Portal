eafa9155da6ea196274720eb0529876739a69b45
Riddhi816 <71248561+Riddhi816@users.noreply.github.com>
2024-06-09 11:15:09 +0200

    modified restaurantlist

--- frontend/rating-portal/src/components/Restaurant/RestaurantList.js
+++ frontend/rating-portal/src/components/Restaurant/RestaurantList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom"; 
 import background from "../../images/background.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const RestaurantList = () => {
     const [restaurants, setRestaurants] = useState([]);
