d060201f7d2d01d833fcce3eb9983448a67363fd
Riddhi816 <71248561+Riddhi816@users.noreply.github.com>
2024-06-11 14:01:50 +0200

    added star average

--- frontend/rating-portal/src/components/Restaurant/RestaurantList.js
+++ frontend/rating-portal/src/components/Restaurant/RestaurantList.js
@@ -1,90 +1,126 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from "react-router-dom"; 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import background from "../../images/background.jpg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './RestaurantList.css';
+import "./RestaurantList.css";
 import SearchBar from "../SearchBar/SearchBar";
+import RatingComponent from "../Ratings/RatingComponent";
+import { fetchAverageRatings } from "../../apiService";
 
 const RestaurantList = () => {
-    const [restaurants, setRestaurants] = useState([]);
-    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-    const [searchQuery, setSearchQuery] = useState(""); 
-    const navigate = useNavigate(); 
+  const [restaurants, setRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [averageRatings, setAverageRatings] = useState({});
+  const navigate = useNavigate();
 
-useEffect(() => {
-        const fetchRestaurants = async () => {
-            try {
-                const response = await axios.post('http://127.0.0.1:5000/restaurants/fetch_all', {}, {
-                    headers: {
-                        'Authorization': `Basic ${btoa('api_gateway:Xe812C81M9yA')}`,
-                        'Content-Type': 'application/json'
-                    }
-                });
-                setRestaurants(response.data);
-            } catch (error) {
-                console.error('Error fetching restaurants:', error);
-            }
-        };
-
-        fetchRestaurants();
-    }, []);
-
-useEffect(() => {
-        const results = restaurants.filter(restaurant =>
-            restaurant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:5000/restaurants/fetch_all",
+          {},
+          {
+            headers: {
+              Authorization: `Basic ${btoa("api_gateway:Xe812C81M9yA")}`,
+              "Content-Type": "application/json",
+            },
+          }
         );
-        setFilteredRestaurants(results);
-    }, [searchQuery, restaurants]);
+        setRestaurants(response.data);
+        const avgRatings = await fetchAverageRatings();
+        setAverageRatings(avgRatings);
+      } catch (error) {
+        console.error("Error fetching restaurants:", error);
+      }
+    };
 
-const SliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    };   
+    fetchRestaurants();
+  }, []);
 
-return (
-    <div className="bg-container" style={{
-            background: `url(${background}) no-repeat center center fixed`,
-            backgroundSize: "cover",
-        }}>
-            <div className="container mt-4">
-                <h1 className="text-center mb-4">Restaurants Near You</h1>
-                <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-                {filteredRestaurants.length > 0 ? (
-                <div className="row">
-                    {filteredRestaurants.map(restaurant => (
-                        <div className="col-md-4 mb-4" key={restaurant.pk_restaurant}>
-                            <div className="card" onClick={() => navigate(`/restaurant/${restaurant.pk_restaurant}`)}>
-                            <Slider {...SliderSettings}>
-                                    {restaurant.images.map((image, index) => (
-                                        <div key={index}>
-                                            <img src={image} className="card-img-top" alt={restaurant.name} />
-                                        </div>
-                                    ))}
-                            </Slider>
-                                <div className="card-body">
-                                    <h5 className="card-title">{restaurant.name}</h5>
-                                    <p className="card-text">Genre: {restaurant.genre}</p>
-                                    <p className="card-text">Address: {`${restaurant.house_number} ${restaurant.street_name}, ${restaurant.city}`}</p>
-                                    {restaurant.verified ? <span className="badge bg-success">Verified</span> : <span className="badge bg-danger">Not Verified</span>}
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                ) :(
-                    <p className="text-center">No Restaurants Found</p>
-                )}
-            </div>
-        </div>
+  useEffect(() => {
+    const results = restaurants.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
+    setFilteredRestaurants(results);
+  }, [searchQuery, restaurants]);
+
+  const SliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
+  return (
+    <div
+      className="bg-container"
+      style={{
+        background: `url(${background}) no-repeat center center fixed`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="container mt-4">
+        <h1 className="text-center mb-4">Restaurants Near You</h1>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        {filteredRestaurants.length > 0 ? (
+          <div className="row">
+            {filteredRestaurants.map((restaurant) => (
+              <div className="col-md-4 mb-4" key={restaurant.pk_restaurant}>
+                <div
+                  className="card"
+                  onClick={() =>
+                    navigate(`/restaurant/${restaurant.pk_restaurant}`)
+                  }
+                >
+                  <Slider {...SliderSettings}>
+                    {restaurant.images.map((image, index) => (
+                      <div key={index}>
+                        <img
+                          src={image}
+                          className="card-img-top"
+                          alt={restaurant.name}
+                        />
+                      </div>
+                    ))}
+                  </Slider>
+                  <div className="card-body">
+                    <h5 className="card-title">{restaurant.name}</h5>
+                    <p className="card-text">Genre: {restaurant.genre}</p>
+                    <p className="card-text">
+                      Address:{" "}
+                      {`${restaurant.house_number} ${restaurant.street_name}, ${restaurant.city}`}
+                    </p>
+                    <RatingComponent
+                      rating={
+                        parseFloat(averageRatings[restaurant.pk_restaurant]) ||
+                        0
+                      }
+                      isClickable={false}
+                      displayNumeric={true}
+                    />
+                    {restaurant.verified ? (
+                      <span className="badge bg-success">Verified</span>
+                    ) : (
+                      <span className="badge bg-danger">Not Verified</span>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No Restaurants Found</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantList;
